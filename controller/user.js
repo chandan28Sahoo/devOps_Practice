762cd3a50418b336685dc732ed3705cb03dbd73a
Ubuntu <ubuntu@ip-172-31-91-16.ec2.internal>
2023-01-10 12:26:12 +0000

    add changes

--- controller/user.js
+++ controller/user.js
@@ -4,7 +4,7 @@ module.exports.getUser = (req,res) =>{
     try {
         res.send ({
             error: false,
-            message: "Hello from Controller"
+            message: "Hello from Controller/user-service"
         })
     } catch (error) {
         return error;
